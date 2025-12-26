0ab0269d8dd7915768aed467e8ded9cd8f450631
Mira-23 <stanimira23@abv.bg>
2025-12-26 14:17:57 +0200

    test-commit

--- PetVerse/Models/Post.cs
+++ PetVerse/Models/Post.cs
@@ -3,7 +3,7 @@ namespace PetVerse.Models
     public class Post
     {
         public int Id { get; set; }
-        public string? Content { get; set; }
+        public required string Content { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
