e92d3807cf0ae94393d4489c7cee3b7f0a601a3a
Mira-23 <80602490+Mira-23@users.noreply.github.com>
2026-01-29 19:34:59 +0200

    added user controller and updated jwt generation/validation (#5)
    added UserController, added Id to JWT token, upped Authentication and added swagger Authorization (currently for testing get_user)

---
+++ PetVerse/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PetVerse.Entities;
+
+namespace PetVerse.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("get_user")]
+        public async Task<IActionResult> GetUser()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId == null)
+                return Unauthorized();
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(new
+            {
+                user.UserName,
+                user.FirstName,
+                user.LastName
+            });
+        }
+    }
+}
