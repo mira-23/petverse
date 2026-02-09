34c2adfc0b3b32fedf7e36304c6bae2b3048cc77
Mira-23 <80602490+Mira-23@users.noreply.github.com>
2026-02-10 00:53:25 +0200

    fixed BusinessProfile being an abstract class, removed whitespace from pet

--- PetVerse/Models/BusinessProfile.cs
+++ PetVerse/Models/BusinessProfile.cs
@@ -1,6 +1,6 @@
 namespace PetVerse.Models
 {
-    public abstract class BusinessProfile : Profile
+    public class BusinessProfile : Profile
     {
         public string? IdendificationNumber { get; set; }
     }
--- PetVerse/Models/Pet.cs
+++ PetVerse/Models/Pet.cs
@@ -2,7 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PetVerse.Models
 {
-
     public enum Kind
     {
         Dog,
