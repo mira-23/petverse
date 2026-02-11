73934696ed61f694daf0a07df9f8473f0f197874
Mira-23 <80602490+Mira-23@users.noreply.github.com>
2026-02-11 22:31:41 +0200

    added missing namespace and removed another redundant dependency
    - removed annotations dependency from pet
    - added usertobusinessprofilemapping to the models namespace

--- PetVerse/Models/UserToBusinessProfileMapping.cs
+++ PetVerse/Models/UserToBusinessProfileMapping.cs
@@ -1,15 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using PetVerse.Entities;
-using PetVerse.Models;
 
-[PrimaryKey(nameof(UserId), nameof(BusinessProfileId))]
-public class UserToBusinessProfileMapping
+namespace PetVerse.Models
 {
-    public string? UserId { get; set; }
+    [PrimaryKey(nameof(UserId), nameof(BusinessProfileId))]
+    public class UserToBusinessProfileMapping
+    {
+        public string? UserId { get; set; }
 
-    public int BusinessProfileId { get; set; }
+        public int BusinessProfileId { get; set; }
 
-    public virtual User? User {get;set;}
-    public virtual BusinessProfile? BusinessProfile {get;set;}
+        public virtual User? User {get;set;}
+        public virtual BusinessProfile? BusinessProfile {get;set;}
 
+    }
 }
