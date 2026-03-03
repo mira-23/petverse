97b8851561db89c483bb08e587c6f3869a2cb1dd
Mira-23 <80602490+Mira-23@users.noreply.github.com>
2026-03-03 23:56:48 +0200

    dto creation
    - made creation dto for adoption request
    - made response dto for adoption request

---
+++ PetVerse/DTOs/AdoptionRequestResponseDTO.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+
+namespace PetVerse.DTOs
+{
+    public class AdoptionRequestResponseDTO
+    {
+        public int Id { get; set; }
+        public int AdoptionPostId { get; set; }
+        public required string Message { get; set; }
+        [Description("Must be either 'new', 'accepted' or 'rejected'")]
+        public required string Status { get; set; }
+    }
+}
