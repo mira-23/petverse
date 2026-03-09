dd5219962951b46bf664e00a19d8e84a6548a225
Mira-23 <80602490+Mira-23@users.noreply.github.com>
2026-03-10 00:06:21 +0200

    added new api call for all adoption posts of a given shelter (#18)
    - changed animalAdoptionResponseDto to have an id and to list its adoption requests (dtos)
    - added findAllAnimalAdoptionPosts to service which gets all adoption posts of a shelter and returns them as dtos
    - added getAnimalAdoptionPosts which returns that list sorted by date
    - added Id to adoption post response dto when a post is created
    - added the endpoint method to list the posts

--- PetVerse/DTOs/AnimalAdoptionResponseDTO.cs
+++ PetVerse/DTOs/AnimalAdoptionResponseDTO.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel;
+using PetVerse.Models;
 
 namespace PetVerse.DTOs
 {
     public class AnimalAdoptionPostRepsonseDTO : PostRepsonseDTO
     {
+        public int Id { get; set;}
         public required string PhotoPath { get; set; }
 
         [Description("Must be either 'cat', 'dog' or 'other'")]
@@ -11,5 +13,6 @@ namespace PetVerse.DTOs
         public int ShelterId { get; set; }
         public DateTime? AdoptedAt { get; set; }
         public required string Status { get; set; }
+        public List<AdoptionRequestResponseDTO>? AdoptionRequestResponseDTOs { get; set; }
     }
 }
